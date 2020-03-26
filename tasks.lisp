4e2297b27f8b55703e8bea2dfcfe61d59fcb3ebc
Karbovskaya <40578231+Karbovskaya@users.noreply.github.com>
2020-03-26 11:26:42 +0300

    Update tasks.lisp

--- tasks.lisp
+++ tasks.lisp
@@ -13,4 +13,21 @@
       )
   )
 
-(inc-list '(2 3 4))
+(print(inc-list '(2 3 4)))
+
+; 21 Определите функцию, удаляющую из списка первое вхождение данного элемента на верхнем уровне.
+
+(defun Del (mainList forDelete)
+   ((lambda (head tail) 
+      (cond 
+         ((null mainList) nil)
+         ((equal head forDelete) tail)
+         (t (cons head (Del tail forDelete)))
+      )
+    )
+    (car mainList)
+    (cdr mainList)
+   )
+)
+(print( Del '((1 2 3) 4 5 (1 2 3) 6)  '(1 2 3))) ;
+
