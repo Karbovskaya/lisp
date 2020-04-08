620cd58d47f24aa2bcb7de15e32770175a590f9f
Karbovskaya <40578231+Karbovskaya@users.noreply.github.com>
2020-04-08 12:42:12 +0300

    Update tasks.lisp

--- tasks.lisp
+++ tasks.lisp
@@ -31,3 +31,35 @@
 )
 (print( Del '((1 2 3) 4 5 (1 2 3) 6)  '(1 2 3))) ;
 
+;45. Напишите функцию (РАССТОЯНИЕ a b), вычисляющую расстояние между городами а и b.
+
+(defun Road (city1 &optional(city2 `Bahchisaray)) 
+  (sqrt (+ (expt (- (get city1 'x) (get city2 'x)) 2)
+           (expt (- (get city1 'y) (get city2 'y)) 2))
+  )
+)
+ 
+(defun City(Name-of-the-city &key x y ) 
+    (setf (get Name-of-the-city 'x) x)
+    (setf (get Name-of-the-city 'y) y)
+  
+)
+
+(City  'New_York :x 1770 :y -445)
+(City  'Simferopol :x -300 :y 50)
+
+
+(print(Road 'New_York 'Simferopol))
+(print(Road 'Simferopol 'New_York))
+
+
+; 28. Определите функцию, вычисляющую, сколько всего атомов в списке (списочной структуре).
+
+(defun atomCnt (l)
+  (cond ((null l) 0)
+    ((atom (car l)) (+ 1 (atomCnt (cdr l))))
+    (t (atomCnt (cdr l)))))
+	
+(print(atomCnt '(2 91 3)))
+(print(atomCnt '(2 (4) 1 1)))
+
